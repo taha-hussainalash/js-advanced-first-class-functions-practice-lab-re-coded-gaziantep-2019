3d26cc4080f8bb39242d8cedf4ebe326af3174eb
Taha HussainAlash <taha.benzma94@gmail.com>
2019-07-21 13:20:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+function logDriverNames(arr){
+arr.forEach(function(element) {
+  console.log(element.name);
+});
 
+   }
+
+   function logDriversByHometown(arr,obj){
+arr.forEach(function(element) {
+  if(element.hometown===obj){
+  console.log(element.name);}
+});
+
+   }
+  function driversByRevenue(arr){
+
+     arr.sort(
+      function (a, b) {return a.revenue - b.revenue;}
+      );
+  return arr;
+ }
+ function driversByName(arr){
+ arr.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+   // names must be equal
+  return 0;
+});
+return arr;
+} 
